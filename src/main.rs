954767e3feac1215450e3262550811ba46698d48
bverhoeve <46007524+bverhoeve@users.noreply.github.com>
2021-03-21 13:20:53 +0100

    finished game

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("What's your guess?");
 
-    // variables are immutable by default
-    // the mut keyword makes the variable mutable
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    loop {
+        println!("What's your guess?");
+    
+        // variables are immutable by default
+        // the mut keyword makes the variable mutable
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
         println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
